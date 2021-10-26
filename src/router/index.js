2be3b8e354bcbebd9384a2f3a57161e5929dac22
unknown <you@example.com>
2021-10-26 15:31:15 +0800

    更新路由滚动

--- src/router/index.js
+++ src/router/index.js
@@ -70,7 +70,10 @@ const routes = [
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    return { top: 0 }
+  }
 })
 
 export default router
