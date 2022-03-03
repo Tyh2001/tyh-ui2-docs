990d6c41dbe15cee9a1da59a630b401393bd079e
15227241216 <1469442737@qq.com>
2022-03-03 21:57:23 +0800

    docs: 更新

--- src/views/tyhUi/index.js
+++ src/views/tyhUi/index.js
@@ -282,8 +282,6 @@ const card = {
     `,
   car2: `
 <tyh-card simple>
-  <template v-slot:title>主标题</template>
-  <template v-slot:subtitle>副标题</template>
   <p>这是内容部分1</p>
   <p>这是内容部分2</p>
   <p>这是内容部分3</p>
